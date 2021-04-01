f1ec4f72ff70b60eba069f5f3ab71f5057eb3615
Regson Do Rego <32569720+RegsonDR@users.noreply.github.com>
2021-04-01 19:02:18 +0100

    feat: script to obtain tweet + replies

---
+++ src/get_tweets_large2.py
@@ -0,0 +1,24 @@
+import tweepy
+import pandas as pd
+import snscrape.modules.twitter as sntwitter
+
+consumer_key = 'hnGPJx6xBsudTgwOAxd0UNGUW'
+consumer_secret = 'JcxhisQeF19hlEHRdeBifU3aL4R9DP9BWEMjYJ7MtnGcWaxn0p'
+access_token = '1249286455530123264-5LbTPxHcE8Hl3CHhWztOsycMGEViHT'
+access_token_secret = '4vdHhFP3odtZSUAKkK6GgPm7Vl03EHwvbP7cRTLFmQbFf'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+RAW_CSV_NAME = "raw_tweets_celeb2.csv"
+tweets_df = pd.DataFrame(columns=["id", "date", "content", "username", "retweet_counter", "favorite_count", "reply_count", "user_followers_count","country"])
+tweets_df.to_csv(RAW_CSV_NAME, quotechar='"', encoding='utf8', index = False, header=True)
+
+maxTweets = 50000
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper('#FarmersProtest' + '-filter:retweets filter:verified min_retweets:2400 since:2020-09-01 until:2021-03-21').get_items()):
+    if i > maxTweets:
+        break
+    print(i)
+    tweets_df = tweets_df = pd.DataFrame(data=[[tweet.id, tweet.date, tweet.content, tweet.username, tweet.retweetCount, tweet.likeCount, tweet.replyCount, tweet.user.followersCount, tweet.user.location]])
+    tweets_df.to_csv(RAW_CSV_NAME, quotechar='"', mode='a', encoding='utf8', index=False, header=False)
